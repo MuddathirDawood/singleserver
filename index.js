c2fb1a774c95e88160f9d876fd53aa38ce867c0e
MuddathirDawood <dmuddathir@gmail.com>
2022-08-01 09:38:31 +0200

    sup

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000);
+app.listen(3000, ()=>{console.log('Server is running at port 3000')});
 
 let courses = [
     {id: 1, name: 'Software Engineering'},
@@ -14,10 +14,15 @@ app.get('/', (req, res)=>{
     res.send(JSON.stringify(courses));
 })
 
+app.get('/courses', (req, res)=>{
+    res.redirect('/');
+})
+
 app.get('/courses/:id',(req, res)=>{
-    if (req.params.id > courses.length) {
+    const index = parseInt(req.params.id) -1
+    if (index > courses.length) {
         res.send('<h1>There is no record with this id</h1>')
     } else {
-        res.send(JSON.stringify(courses[req.params.id - 1]));
+        res.send(JSON.stringify(courses[index]));
     }
 })
